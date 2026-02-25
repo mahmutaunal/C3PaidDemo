55a8677fea78b00252e7129e45bac1ab4681f539
Mahmut Alperen Ünal <mahmutalperenunal@gmail.com>
2026-02-25 14:18:53 +0300

    chore: add navigation, viewmodel and datastore dependencies
    Add Navigation-Compose, lifecycle ViewModel compose, and DataStore to support MVVM and screen navigation.

---
+++ app/src/main/java/com/mahmutalperenunal/c3paiddemo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mahmutalperenunal.c3paiddemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mahmutalperenunal.c3paiddemo.ui.theme.C3PaidDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            C3PaidDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    C3PaidDemoTheme {
+        Greeting("Android")
+    }
+}
